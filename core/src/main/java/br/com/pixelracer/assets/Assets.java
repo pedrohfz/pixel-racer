ab7e585f1947d5f24eee60f598b6ebbd97513753
Pedro Leite <pedrohleite2005@gmail.com>
2025-10-28 22:23:59 -0300

    feat: adição de uma fonte pixelada para o jogo

--- core/src/main/java/br/com/pixelracer/assets/Assets.java
+++ core/src/main/java/br/com/pixelracer/assets/Assets.java
@@ -1,15 +1,34 @@
 package br.com.pixelracer.assets;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 public class Assets {
     public BitmapFont fontSmall;
     public BitmapFont fontBig;
 
     public void load() {
-        fontSmall = new BitmapFont();
-        fontBig   = new BitmapFont();
-        fontBig.getData().setScale(1.4f);
+        FreeTypeFontGenerator gen = new FreeTypeFontGenerator(Gdx.files.internal("assets/fonts/PressStart2P.ttf"));
+
+        FreeTypeFontParameter ps = new FreeTypeFontParameter();
+        ps.size = 16;
+        ps.color = Color.WHITE;
+        ps.minFilter = Texture.TextureFilter.Nearest;
+        ps.magFilter = Texture.TextureFilter.Nearest;
+
+        FreeTypeFontParameter pb = new FreeTypeFontParameter();
+        pb.size = 24;
+        pb.color = Color.WHITE;
+        pb.minFilter = Texture.TextureFilter.Nearest;
+        pb.magFilter = Texture.TextureFilter.Nearest;
+
+        fontSmall = gen.generateFont(ps);
+        fontBig   = gen.generateFont(pb);
+        gen.dispose();
     }
 
     public void dispose() {
